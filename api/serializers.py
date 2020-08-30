a429f61b0d72d2d6c8c8ca5a9a8ca4fec7d11cb3
Z-VV <44175275+Z-VV@users.noreply.github.com>
2020-08-30 14:53:01 +0100

    Delete serializers.py

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Signals
-
-class HeroSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Signals
-        fields = ('signal_type','symbol','buy_sell','stop_loss','take_profit','number','provider','created')
-
